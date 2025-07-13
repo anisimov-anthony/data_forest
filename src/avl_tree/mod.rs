d5067408759e36016a69978dd11c377676acc5f3
AntonAnisimovMIPT <anisimov-anton-main@mail.ru>
2025-07-13 16:47:39 +0300

    remove 2 unneccessary impls

--- src/avl_tree/mod.rs
+++ src/avl_tree/mod.rs
@@ -12,19 +12,3 @@ pub struct AVLTree<T: PartialOrd + Clone> {
     min_value: Option<T>,
     max_value: Option<T>,
 }
-
-impl<T: Ord + Clone> AVLTree<T> {
-    pub fn find_nodes_with_heights(&self) -> Vec<(T, usize)> {
-        let mut nodes = Vec::new();
-        Self::collect_nodes(&self.root, &mut nodes);
-        nodes
-    }
-
-    fn collect_nodes(node: &Option<Box<AVLNode<T>>>, nodes: &mut Vec<(T, usize)>) {
-        if let Some(node) = node {
-            nodes.push((node.value.clone(), node.height));
-            Self::collect_nodes(&node.left, nodes);
-            Self::collect_nodes(&node.right, nodes);
-        }
-    }
-}
